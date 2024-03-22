fd0533d08d3aaad1e3262013a1f3396b98dd92b9
NourTarek201 <92692198+NourTarek201@users.noreply.github.com>
2024-03-22 07:41:14 +0200

    done 3,6,12

--- Assignment1AI.pl
+++ Assignment1AI.pl
@@ -1,5 +1,20 @@
-
+# Assignment1AI
 :-consult(data).
+:-dynamic
+    item/3,
+    alternative/2,
+    boycott_company/2.
+
+%3
+getItemsInOrderById(CustName,OrderId, Items):-
+    customer(Id, CustName),
+    order(Id, OrderId, Items).
+
+%6
+isBoycott(Name):-
+    item(Name,Company, _) -> boycott_company(Company, _);
+    boycott_company(Name, _ ).
+
 
 % 9
 replaceBoycottItemsFromAnOrder(UserName, OrderID, NewList) :-
@@ -14,15 +29,8 @@ replaceBoycottItems([Item|T], [NewItem|NewT]) :-
     ;   NewItem = Item
     ),
     replaceBoycottItems(T, NewT).
-%11
-getTheDifferenceInPriceBetweenItemAndAlternative(Item,A,DiffPrice):-
-    alternative(Item, A),
-    priceOfItem(Item,ItemPrice),
-    priceOfItem(A, AlternativePrice),
-    DiffPrice is ItemPrice -  AlternativePrice .
 
-priceOfItem(Item, Price) :-
-    item(Item,_, Price).
+
 %10
 calcPriceAfterReplacingBoycottItemsFromAnOrder(UserName, OrderID, NewList, TotalPrice) :-
       replaceBoycottItemsFromAnOrder(UserName, OrderID, NewList),
@@ -32,3 +40,37 @@ calculateTotalPrice([Item|T], TotalPrice) :-
     priceOfItem(Item, Price),
     calculateTotalPrice(T, RemainingPrice),
     TotalPrice is Price + RemainingPrice.
+
+%11
+getTheDifferenceInPriceBetweenItemAndAlternative(Item,A,DiffPrice):-
+    alternative(Item, A),
+    priceOfItem(Item,ItemPrice),
+    priceOfItem(A, AlternativePrice),
+    DiffPrice is ItemPrice -  AlternativePrice .
+
+priceOfItem(Item, Price) :-
+    item(Item,_, Price).
+
+
+
+% bonus 12
+add_item(Name, Company, ID):-
+    assert(item(Name, Company, ID)).
+remove_item(Name, Company, ID):-
+    retract(item(Name, Company, ID)).
+
+add_alternative(Item, Alter):-
+    assert(alternative(Item, Alter)).
+remove_alternative(Item, Alter):-
+    retract(alternative(Item, Alter)).
+
+add_boycott_company(Company, Text):-
+    assert(boycott_company(Company, Text)).
+remove_boycott_company(Company, Text):-
+    retract(boycott_company(Company, Text)).
+
+
+
+
+
+
