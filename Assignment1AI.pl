4e9d74d7b0f287f1e724cdf4211f5424dab386ff
NourTarek201 <92692198+NourTarek201@users.noreply.github.com>
2024-03-22 07:40:27 +0200

    Revert "completed 3,6,12"
    This reverts commit 1d50fc8a6da4e68f654823897ebab00ead26fc4f.

--- Assignment1AI.pl
+++ Assignment1AI.pl
@@ -1,20 +1,5 @@
-# Assignment1AI
+
 :-consult(data).
-:-dynamic
-    item/3,
-    alternative/2,
-    boycott_company/2.
-
-%3
-getItemsInOrderById(CustName,OrderId, Items):-
-    customer(Id, CustName),
-    order(Id, OrderId, Items).
-
-%6
-isBoycott(Name):-
-    item(Name,Company, _) -> boycott_company(Company, _);
-    boycott_company(Name, _ ).
-
 
 % 9
 replaceBoycottItemsFromAnOrder(UserName, OrderID, NewList) :-
@@ -29,18 +14,6 @@ replaceBoycottItems([Item|T], [NewItem|NewT]) :-
     ;   NewItem = Item
     ),
     replaceBoycottItems(T, NewT).
-
-
-%10
-calcPriceAfterReplacingBoycottItemsFromAnOrder(UserName, OrderID, NewList, TotalPrice) :-
-      replaceBoycottItemsFromAnOrder(UserName, OrderID, NewList),
-      calculateTotalPrice(NewList, TotalPrice).
-calculateTotalPrice([], 0).
-calculateTotalPrice([Item|T], TotalPrice) :-
-    priceOfItem(Item, Price),
-    calculateTotalPrice(T, RemainingPrice),
-    TotalPrice is Price + RemainingPrice.
-
 %11
 getTheDifferenceInPriceBetweenItemAndAlternative(Item,A,DiffPrice):-
     alternative(Item, A),
@@ -50,27 +23,12 @@ getTheDifferenceInPriceBetweenItemAndAlternative(Item,A,DiffPrice):-
 
 priceOfItem(Item, Price) :-
     item(Item,_, Price).
-
-
-
-% bonus 12
-add_item(Name, Company, ID):-
-    assert(item(Name, Company, ID)).
-remove_item(Name, Company, ID):-
-    retract(item(Name, Company, ID)).
-
-add_alternative(Item, Alter):-
-    assert(alternative(Item, Alter)).
-remove_alternative(Item, Alter):-
-    retract(alternative(Item, Alter)).
-
-add_boycott_company(Company, Text):-
-    assert(boycott_company(Company, Text)).
-remove_boycott_company(Company, Text):-
-    retract(boycott_company(Company, Text)).
-
-
-
-
-
-
+%10
+calcPriceAfterReplacingBoycottItemsFromAnOrder(UserName, OrderID, NewList, TotalPrice) :-
+      replaceBoycottItemsFromAnOrder(UserName, OrderID, NewList),
+      calculateTotalPrice(NewList, TotalPrice).
+calculateTotalPrice([], 0).
+calculateTotalPrice([Item|T], TotalPrice) :-
+    priceOfItem(Item, Price),
+    calculateTotalPrice(T, RemainingPrice),
+    TotalPrice is Price + RemainingPrice.
